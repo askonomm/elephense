917d3d443f41d6630a9c198d4792f79ecfa7cdc3
Kristófer R <kristofer.thorlaksson@automattic.com>
2022-10-26 19:41:44 -0400

    add utility function for getting path to bundled client

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,13 @@
 export const getConfiguredIntelephensePath = () => {
 	return nova.config.get('intelephense.language-server-path', 'string');
 };
+
+export const getBundledIntelephensePath = () => {
+	return nova.path.join(
+		nova.extension.path,
+		'node_modules',
+		'intelephense',
+		'lib',
+		'intelephense.js'
+	);
+};
